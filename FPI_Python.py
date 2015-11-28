35c3b4766f341cedcfadd94887a96c6986722962
bocattelan <bruno_cattelan02@hotmail.com>
2015-11-28 13:47:04 -0200

    testes com multiplicacao de imagens

--- FPI_Python.py
+++ FPI_Python.py
@@ -1,18 +1,29 @@
 from skimage import data, io, filters
 from skimage.color import rgb2gray
 from skimage.util import img_as_ubyte
+from skimage.transform import resize
 
 
 
+foreground = io.imread('C:\Users\Bruno\Workspace\CamouflageImages\\eu.png')
+background = io.imread('C:\Users\Bruno\Workspace\CamouflageImages\\banaSplit.jpg')
 
-img = io.imread('C:\Users\Bruno\Workspace\CamouflageImages\\eu.png')
+#foreground = resize(foreground, (640, 640))
+
 #into grayscale
-img_gray = rgb2gray(img)
+foreground_gray = rgb2gray(foreground)
+background_gray = rgb2gray(background) 
 
 #quantization
-quantized = img_as_ubyte(img_gray) 
-quantized = quantized // 32
+foreground_quantized = img_as_ubyte(foreground_gray) 
+foreground_quantized = foreground_quantized // 8
+
+background_quantized = img_as_ubyte(background_gray) 
+background_quantized = background_quantized // 8
 
 
-io.imshow(quantized, cmap='gray')
+
+#(100, 100)
+merged = foreground_quantized * background_quantized
+io.imshow(merged, cmap='gray')
 io.show()
