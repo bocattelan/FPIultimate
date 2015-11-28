69c5fa55424e0b2b8730233fc436eaa41e709fa7
bocattelan <bruno_cattelan02@hotmail.com>
2015-11-28 12:57:45 -0200

    comentarios

--- FPI_Python.py
+++ FPI_Python.py
@@ -6,8 +6,13 @@ from skimage.util import img_as_ubyte
 
 
 img = io.imread('C:\Users\Bruno\Workspace\CamouflageImages\\eu.png')
+#into grayscale
 img_gray = rgb2gray(img)
+
+#quantization
 quantized = img_as_ubyte(img_gray) 
 quantized = quantized // 32
+
+
 io.imshow(quantized, cmap='gray')
 io.show()
